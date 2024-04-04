923115252c06016ccef6ce14bdd9e985c819cab8
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-04 14:43:18 +0300

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+
+
+export default function About() {
+    return (
+        <div className="about-page">
+            <div className="about-page-container width">
+                <h1>About page</h1>
+            </div>
+        </div>
+    )
+}
