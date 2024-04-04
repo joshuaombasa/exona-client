985fff640f24202d0d4a2fdc7dcd27135e9d9a5d
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-04 14:43:47 +0300

    initial commit

---
+++ src/pages/host/Income.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Income() {
+    return (
+        <div className="income-page">
+            <div className="income-page-container width">
+                <h1>Income</h1>
+            </div>
+        </div>
+    )
+}
