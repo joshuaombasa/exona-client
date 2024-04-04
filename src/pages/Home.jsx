ab49b324b5494f82bc7670fd2a85011c551a86f6
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-04 14:43:11 +0300

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+    return (
+        <div className="home-page">
+            <div className="home-page-container width">
+                <h1>Home page</h1>
+            </div>
+        </div>
+    )
+}
