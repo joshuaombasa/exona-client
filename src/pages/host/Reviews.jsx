b66f3fed33f67937c334c0ad9b3a1f0866c0c2c3
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-04 14:43:27 +0300

    initial commit

---
+++ src/pages/host/Reviews.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Reviews() {
+    return (
+        <div className="reviews-page">
+            <div className="reviews-page-container width">
+                <h1>reviews</h1>
+            </div>
+        </div>
+    )
+}
