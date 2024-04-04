72afe678d3eaabc050f3dc74e66c927baf11fd26
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-04 14:42:52 +0300

    initial commit

---
+++ src/pages/Properties.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Properties() {
+    return (
+        <div className="properties-page">
+            <div className="properties-page-container width">
+                <h1>properties page</h1>
+            </div>
+        </div>
+    )
+}
