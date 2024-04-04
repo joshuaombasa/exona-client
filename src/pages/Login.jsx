62f7bc4a50fb7f78398c750b3dedf081c373f204
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-04 14:43:03 +0300

    initial commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Login() {
+    return (
+        <div className="login-page">
+            <div className="login-page-container width">
+                <h1>Login page</h1>
+            </div>
+        </div>
+    )
+}
