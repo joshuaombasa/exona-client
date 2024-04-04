d3eb77907cfae48f7f64b29e294d5ef645e403c0
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-04 14:43:36 +0300

    initial commit

---
+++ src/pages/host/HostProperties.jsx
@@ -0,0 +1,11 @@
+
+
+export default function HostProperties() {
+    return (
+        <div className="host-properties">
+            <div className="host-properties width">
+                <h1>HostProperties</h1>
+            </div>
+        </div>
+    )
+}
