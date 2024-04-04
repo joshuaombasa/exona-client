e9e89a2af4ed047f52eb9773eae854f2e1278854
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-04 14:43:55 +0300

    initial commit

---
+++ src/pages/host/Dashboard.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Dashboard() {
+    return (
+        <div className="dashboard-page">
+            <div className="dashboard-page-container  width">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+    )
+}
