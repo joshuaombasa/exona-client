abd677d897823a30250d253181c8a460acbaa084
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-05 18:25:39 +0300

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
     return (
         <div className="home-page">
             <div className="home-page-container width">
-                <h1>Home page</h1>
+                <h1>You got the travel plans, we got the travel vans.</h1>
+                <h2>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</h2>
+                <Link>Find your van</Link>
             </div>
         </div>
     )
