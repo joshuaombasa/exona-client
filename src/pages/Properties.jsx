0fc578ad535436061e8764c6b06db11ef2bf066c
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-05 18:25:19 +0300

    initial commit

--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -1,11 +1,42 @@
-
+import propertyImage from '../assets/about.jpg'
 
 
 export default function Properties() {
+
+    const PropertyComponent = () => {
+        return (
+            <div className="property-component">
+                <img src={propertyImage} alt="" />
+                <div className="property-data">
+                    <h3>Modest Explorer</h3>
+                    <div className="price">
+                        <h3>$60</h3>
+                        <h4>/day</h4>
+                    </div>
+                </div>
+                <span>Simple</span>
+            </div>
+        )
+    }
+
     return (
         <div className="properties-page">
             <div className="properties-page-container width">
-                <h1>properties page</h1>
+                <h1>Explore our van options</h1>
+                <nav>
+                    <button>Simple</button>
+                    <button>Luxury</button>
+                    <button>Rugged</button>
+                    <button>Clear filters</button>
+                </nav>
+                <div className="property-list">
+                    <PropertyComponent/>
+                    <PropertyComponent/>
+                    <PropertyComponent/>
+                    <PropertyComponent/>
+                    <PropertyComponent/>
+                    <PropertyComponent/>
+                </div>
             </div>
         </div>
     )
