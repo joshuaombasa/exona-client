07693d8123329c2ad320d8c5486d11be736e8c17
Joshua Ombasa <nyambegaombasa@gmail.com>
2025-03-16 21:18:07 +0300

    updated routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,38 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Layout from "./components/Layout"
-import About from "./pages/About"
-import Home from "./pages/Home"
-import './App.css'
-import Properties from "./pages/Properties"
-import Login from "./pages/Login"
-import HostLayout from "./components/HostLayout"
-import Dashboard from "./pages/host/Dashboard"
-import HostProperties from "./pages/host/HostProperties"
-import Income from "./pages/host/Income"
-import Reviews from "./pages/host/Reviews"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Properties from "./pages/Properties";
+import Login from "./pages/Login";
+import HostLayout from "./components/HostLayout";
+import Dashboard from "./pages/host/Dashboard";
+import HostProperties from "./pages/host/HostProperties";
+import Income from "./pages/host/Income";
+import Reviews from "./pages/host/Reviews";
+import "./App.css";
 
 function App() {
-
-
   return (
-    <div className="">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="properties" element={<Properties />} />
-            <Route path="login" element={<Login />} />
-            <Route path="hosts" element={<HostLayout />}>
-              <Route index element={<Dashboard/>}/>
-              <Route path="properties" element={<HostProperties/>}/>
-              <Route path="income" element={<Income/>}/>
-              <Route path="reviews" element={<Reviews/>}/>
-            </Route>
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="properties" element={<Properties />} />
+          <Route path="login" element={<Login />} />
+
+          {/* Host Dashboard Routes */}
+          <Route path="hosts" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="properties" element={<HostProperties />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
